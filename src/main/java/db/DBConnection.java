d804420599a3929d50383eab77e1df333f3c21ad
Elisa Yan - elisa.yan@studio.unibo.it <elisa.yan@studio.unibo.it>
2024-05-14 16:36:15 +0200

    set up DB connection

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,3 +1,29 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriveManager;
+import java.sql.SQLException;
+
 public class DBConnection {
     
+    public Connection getMySQLConnection(){
+        final String dbName = "train_track";
+
+        final String driver = "com.mysql.cj.jdbc.Driver";
+        final String dbUri = "jdbc:mysql://localhost:3306/" + dbName + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+        final String userName = "root";
+        final String password = "password";
+
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(dbUri, userName, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
