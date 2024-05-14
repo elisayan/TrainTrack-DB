c6d86d3dd1bcbef61a271fc50f7930cae8688c3d
Elisa Yan <elisa.yan@studio.unibo.it>
2024-05-14 22:27:38 +0200

    setup gradle

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    //id("org.danilopianini.gradle-java-qa") version "1.50.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.5")
+
+    // Example library: Guava. Add what you need (and remove Guava if you don't use it)
+    // implementation("com.google.guava:guava:28.1-jre")
+
+    // JavaFX: comment out if you do not need them
+    val javaFxVersion = 15
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("com.hynnet:sqljdbc4-chs:4.0.2206.100")
+    implementation("net.sourceforge.jtds:jtds:1.3.1")
+    val jUnitVersion = "5.10.2"
+    // JUnit API and testing engine
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+val main: String by project
+
+application {
+    // Define the main class for the application
+    mainClass.set(main)
+}
