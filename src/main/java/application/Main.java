85ce6b23b9455787d8cf1acb12a991bf913dc4c4
Elisa Yan - elisa.yan@studio.unibo.it <elisa.yan@studio.unibo.it>
2024-05-14 17:17:45 +0200

    added main function

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,3 +1,11 @@
-public class Main {
-    
+package application;
+
+public final class Main{
+    private Main(){
+
+    }
+
+    public static void main (final String[] args) {
+        System.out.println("Hello")
+    }
 }
