a404143da0ac6beecf437f88eeb8968ee25cd6d3
Elisa Yan <elisa.yan@studio.unibo.it>
2024-05-16 11:50:49 +0200

    corrected model

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Model {
+public class Model{
+
+    private User user;
+
+    public void setUser(final User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
     
 }
