e1e9511780b0e43c9101424438141914744b5d0c
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-07 16:29:53 +0200

    implemented login user

--- src/main/java/view/controller/LoginSceneController.java
+++ src/main/java/view/controller/LoginSceneController.java
@@ -1,7 +1,13 @@
 package view.controller;
 
+import controller.LoginController;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -10,13 +16,22 @@ import model.Person;
 public class LoginSceneController extends AbstractSceneController {
 
     @FXML
-    private Button login;
+    private Button loginButton;
 
     @FXML
-    private Button home;
+    private Button homeButton;
 
     @FXML
     private Button signButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Label errorLabel;
 
     @FXML
     private AnchorPane pane;
@@ -27,8 +42,26 @@ public class LoginSceneController extends AbstractSceneController {
     @FXML
     private HBox hBox;
 
+    private LoginController controller;
+
+    private EventHandler<MouseEvent> login =(l)->{
+        if (!this.isFieldsEmpty()) {
+            Platform.runLater(()->{
+                this.controller.loginPerson(emailField.getText(), passwordField.getText());
+            });
+        } else{
+            errorLabel.setText(MessageError.EMPTY_FIELD.toString());
+        }
+    };
+
+    private boolean isFieldsEmpty() {
+        return emailField.getText().isBlank() || passwordField.getText().isBlank();
+    }
+
     @FXML
     public void loginClicked() {
+        loginButton = new Button();
+        loginButton.setOnMouseClicked(login);
         this.view.switchScene("home.fxml");
     }
 
@@ -43,5 +76,6 @@ public class LoginSceneController extends AbstractSceneController {
     }
 
     public void loginFailed() {
+        errorLabel.setText(MessageError.ERROR.toString());
     }
 }
