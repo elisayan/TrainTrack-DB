17d9d4b08abf0895f1660497e5b55e2a98cbec8d
Elisa Yan <elisa.yan@studio.unibo.it>
2024-06-10 09:45:09 +0200

    initialized journey ranking

--- src/main/java/view/controller/RankingSceneController.java
+++ src/main/java/view/controller/RankingSceneController.java
@@ -2,11 +2,15 @@ package view.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.Journey;
 
-public class RankingSceneController extends AbstractSceneController{
+public class RankingSceneController extends AbstractSceneController {
 
     @FXML
     private Button homeButton;
@@ -30,18 +34,51 @@ public class RankingSceneController extends AbstractSceneController{
     private HBox hBox;
 
     @FXML
-    private void loginClicked(){
+    private Label delaysLabel;
+
+    @FXML
+    private TableView<Journey> table;
+
+    @FXML
+    private TableColumn<Journey, Integer> delaysJourneyId;
+
+    @FXML
+    private TableColumn<Journey,String> delaysDeparture;
+
+    @FXML
+    private TableColumn<Journey,String> delaysArrival;
+
+    @FXML
+    private TableColumn<Journey,Integer> delaysAverage;
+
+    @FXML
+    private Label earlyLabel;
+
+    @FXML
+    private TableColumn<Journey, Integer> earlyJourneyId;
+
+    @FXML
+    private TableColumn<Journey,String> earlyDeparture;
+
+    @FXML
+    private TableColumn<Journey,String> earlyArrival;
+
+    @FXML
+    private TableColumn<Journey,Integer> earlyAverage;
+
+    @FXML
+    private void loginClicked() {
         this.view.switchScene("login.fxml");
     }
 
     @FXML
-    private void journeyClicked(){
+    private void journeyClicked() {
         this.view.switchScene("journeyRanking.fxml");
     }
 
     @FXML
-    private void expensesClicked(){
+    private void expensesClicked() {
         this.view.switchScene("expensesRanking.fxml");
     }
-    
+
 }
