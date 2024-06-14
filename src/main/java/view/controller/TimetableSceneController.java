f3d8f2b1d69de8822ddd9a90256da329bff4effd
Amiela P Junio <amielap.junio@studio.unibo.it>
2024-06-14 17:23:03 +0200

    fill timetableGridPane with labels

--- src/main/java/view/controller/TimetableSceneController.java
+++ src/main/java/view/controller/TimetableSceneController.java
@@ -8,92 +8,22 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import view.View;
 
 public class TimetableSceneController extends AbstractSceneController {
     
     @FXML
-    private Label departure1Label;
-
-    @FXML
-    private Label departure2Label;
-
-    @FXML
-    private Label departure3Label;
-
-    @FXML
-    private Label departure4Label;
-
-    @FXML
-    private Label departure5Label;
-
-    @FXML
-    private Label departure6Label;
-
-    @FXML
-    private Label departure7Label;
-
-    @FXML
-    private Label departure8Label;
-
-    @FXML
     private Label departureLabel;
-
-    @FXML
-    private Label destination1Label;
-
-    @FXML
-    private Label destination2Label;
-
-    @FXML
-    private Label destination3Label;
-
-    @FXML
-    private Label destination4Label;
-
-    @FXML
-    private Label destination5Label;
-
-    @FXML
-    private Label destination6Label;
-
-    @FXML
-    private Label destination7Label;
-
-    @FXML
-    private Label destination8Label;
 
     @FXML
     private Label destinationLabel;
 
     @FXML
     private Button homeButton;
-
-    @FXML
-    private Label idjourney1Label;
-
-    @FXML
-    private Label idjourney2Label;
-
-    @FXML
-    private Label idjourney3Label;
-
-    @FXML
-    private Label idjourney4Label;
-
-    @FXML
-    private Label idjourney5Label;
-
-    @FXML
-    private Label idjourney6Label;
-
-    @FXML
-    private Label idjourney7Label;
-
-    @FXML
-    private Label idjourney8Label;
 
     @FXML
     private Label idjourneyLabel;
@@ -129,55 +59,7 @@ public class TimetableSceneController extends AbstractSceneController {
     private Label notificationLabel;
 
     @FXML
-    private Label platform1Label;
-
-    @FXML
-    private Label platform2Label;
-
-    @FXML
-    private Label platform3Label;
-
-    @FXML
-    private Label platform4Label;
-
-    @FXML
-    private Label platform5Label;
-
-    @FXML
-    private Label platform6Label;
-
-    @FXML
-    private Label platform7Label;
-
-    @FXML
-    private Label platform8Label;
-
-    @FXML
     private Label platformLabel;
-
-    @FXML
-    private Label status1Label;
-
-    @FXML
-    private Label status2Label;
-
-    @FXML
-    private Label status3Label;
-
-    @FXML
-    private Label status4Label;
-
-    @FXML
-    private Label status5Label;
-
-    @FXML
-    private Label status6Label;
-
-    @FXML
-    private Label status7Label;
-
-    @FXML
-    private Label status8Label;
 
     @FXML
     private Label statusLabel;
@@ -186,31 +68,10 @@ public class TimetableSceneController extends AbstractSceneController {
     private Label timatableLabel;
 
     @FXML
-    private Label time1Label;
-
-    @FXML
-    private Label time2Label;
-
-    @FXML
-    private Label time3Label;
-
-    @FXML
-    private Label time4Label;
-
-    @FXML
-    private Label time5Label;
-
-    @FXML
-    private Label time6Label;
-
-    @FXML
-    private Label time7Label;
-
-    @FXML
-    private Label time8Label;
-
-    @FXML
     private Label timeLabel;
+
+    @FXML
+    private GridPane timetableGridPane;
 
     @FXML
     private AnchorPane pane;
@@ -227,17 +88,47 @@ public class TimetableSceneController extends AbstractSceneController {
     public void initialize(View view, Controller controller) {
         super.initialize(view, controller);
         this.controller = new TimetableController(this, this.getController());
+        
+        int rows = 9;
+        int columns = 7;
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 0; j < columns - 1; j++) {
+                Label label = new Label("Row " + i + " Col " + j);
+                timetableGridPane.add(label, j, i);
+            }
+        }
+
+        //for (int x = 0; x < rows; x++) {
+        //    boolean hasText = false;
+        //    for (int y = 0; y < columns - 1; y++) {
+        //        Text textNode = (Text) getNodeByRowColumnIndex(y, x, timetableGridPane);
+        //        if (textNode != null && !textNode.getText().isEmpty()) {
+        //            hasText = true;
+        //            break;
+        //        }
+        //    }
+
+        //    if (hasText) {
+        //        timetableGridPane.add(new CheckBox(), columns - 1, x);
+        //    }
+        //}
+
     }
 
-    @FXML
-    public void homeClicked() {
-        this.view.switchScene("home.fxml");
-    }
+    //@FXML
+    //private javafx.scene.Node getNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
+    //    for (javafx.scene.Node node : gridPane.getChildren()) {
+    //        if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
+    //            return node;
+    //        }
+    //    }
+    //    return null;
+    //}
 
     @FXML
     public void loginClicked() {
         this.view.switchScene("login.fxml");
     }
-
 
 }
