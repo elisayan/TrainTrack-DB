e32967ac339872d4574b975dbd9b9bff44fc8694
Elisa Yan <elisa.yan@studio.unibo.it>
2024-07-15 12:34:11 +0200

    initialized passenger detail

---
+++ src/main/java/view/controller/PassengerDetailSceneController.java
@@ -0,0 +1,43 @@
+package view.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class PassengerDetailSceneController extends AbstractSceneController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField cfField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private ImageView homeButton;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private ImageView userButton;
+
+
+    @FXML
+    private void confirmClicked() {
+        //verifica se le info sono inserite correttamente
+    }
+
+    @FXML
+    private void userClicked() {
+        this.view.switchScene("login.fxml");
+    }
+}
